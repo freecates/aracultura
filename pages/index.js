076c525b54dbe3741cf752fc4f63dfbf42f5b5ab
Ramon Gil <ramongil@ramongil.cat>
2020-04-23 08:40:00 +0200

    styles to css via import

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,29 @@
+import fetch from 'isomorphic-unfetch';
 import Layout from '../components/Layout';
 
-const Home = () => {
+const Home = ({ data }) => {
+  const { title, content } = data;
   return (
     <>
       <Layout page={'home'}>
-        <h1 className='title'>Ara Cultura!</h1>
+        <h1 className='title'>{title.rendered}</h1>
+        <div
+          className='description'
+          dangerouslySetInnerHTML={{ __html: content.rendered }}
+        />
       </Layout>
     </>
   );
+};
+
+export const getStaticProps = async () => {
+  const res = await fetch(`https://cms.aracultura.com/wp-json/wp/v2/pages/76`);
+  const data = await res.json();
+  return {
+    props: {
+      data: data,
+    },
+  };
 };
 
 export default Home;
