e8ede93bfbe750f28e0c8146312fc933a9e0c66d
ramon.gil <ramon.gil@deporvillage.com>
2020-04-27 10:54:35 +0200

    added uàtzap

--- components/BlocPost.js
+++ components/BlocPost.js
@@ -10,6 +10,8 @@ import {
   LinkedinShareButton,
   TwitterIcon,
   TwitterShareButton,
+  WhatsappIcon,
+  WhatsappShareButton,
 } from 'react-share';
 import styles from './Bloc.module.scss';
 
@@ -40,41 +42,35 @@ const BlocPost = ({
     author = 'Ara Cultura';
   }
   return (
-    <>
-      <Head>
-        <title>
-          {title} - Ara Cultura - {type}
-        </title>
-        <meta name='description' content={description} />
+      <>
+          <Head>
+              <title>
+                  {title} - Ara Cultura - {type}
+              </title>
+              <meta name='description' content={description} />
 
-        <meta property='fb:app_id' content='1064356173625695' />
-        <meta
-          property='og:url'
-          content={`https://www.aracultura.com/${type}/${id}`}
-        />
-        <meta property='og:type' content='article' />
-        <meta property='og:title' content={title} />
-        <meta property='og:description' content={description} />
-        <meta
-          property='og:image'
-          content={`https://www.aracultura.com/icons/og-image-aracultura-web.png`}
-        />
-        <meta property='og:image:width' content={1024} />
-        <meta property='og:image:height' content={1024} />
+              <meta property='fb:app_id' content='1064356173625695' />
+              <meta property='og:url' content={`https://www.aracultura.com/${type}/${id}`} />
+              <meta property='og:type' content='article' />
+              <meta property='og:title' content={title} />
+              <meta property='og:description' content={description} />
+              <meta
+                  property='og:image'
+                  content={`https://www.aracultura.com/icons/og-image-aracultura-web.png`}
+              />
+              <meta property='og:image:width' content={1024} />
+              <meta property='og:image:height' content={1024} />
 
-        <meta name='twitter:card' content='summary_large_image' />
-        <meta name='twitter:site' content='mexico_baila' />
-        <meta name='twitter:creator' content='mexico_baila' />
-        <meta name='twitter:title' content={title} />
-        <meta
-          name='twitter:image:src'
-          content={`${!imatge ? null : imatge.url}`}
-        />
+              <meta name='twitter:card' content='summary_large_image' />
+              <meta name='twitter:site' content='mexico_baila' />
+              <meta name='twitter:creator' content='mexico_baila' />
+              <meta name='twitter:title' content={title} />
+              <meta name='twitter:image:src' content={`${!imatge ? null : imatge.url}`} />
 
-        <script
-          type='application/ld+json'
-          dangerouslySetInnerHTML={{
-            __html: `
+              <script
+                  type='application/ld+json'
+                  dangerouslySetInnerHTML={{
+                      __html: `
 {
   "@context": "http://schema.org",
   "@type": "NewsArticle",
@@ -99,174 +95,188 @@ const BlocPost = ({
   "date": "${date}",
   "headline": "${title}"
 }`,
-          }}
-        />
-      </Head>
-      <div className={styles.bloc}>
-        <IntlProvider locale={'ca'} timeZone='Europe/Madrid'>
-          {!author ? null : (
-            <p>
-              <small>
-                <strong>Per:</strong> {author}. <strong>Publicada el:</strong>{' '}
-                <FormattedDate value={date} />
-              </small>
-            </p>
-          )}
-          {!imatge ? null : (
-            <picture>
-              <img loading='lazy' src={imatge.url} width={'100%'} />
-            </picture>
-          )}
-          <h1 className='title'>{title}</h1>
-          <h2 dangerouslySetInnerHTML={{ __html: description }} />
+                  }}
+              />
+          </Head>
+          <div className={styles.bloc}>
+              <IntlProvider locale={'ca'} timeZone='Europe/Madrid'>
+                  {!author ? null : (
+                      <p>
+                          <small>
+                              <strong>Per:</strong> {author}. <strong>Publicada el:</strong>{' '}
+                              <FormattedDate value={date} />
+                          </small>
+                      </p>
+                  )}
+                  {!imatge ? null : (
+                      <picture>
+                          <img loading='lazy' src={imatge.url} width={'100%'} />
+                      </picture>
+                  )}
+                  <h1 className='title'>{title}</h1>
+                  <h2 dangerouslySetInnerHTML={{ __html: description }} />
 
-          {type == 'post' && (
-            <div
-              className={styles.alignLeft}
-              dangerouslySetInnerHTML={{ __html: content }}
-            />
-          )}
+                  {type == 'post' && (
+                      <div
+                          className={styles.alignLeft}
+                          dangerouslySetInnerHTML={{ __html: content }}
+                      />
+                  )}
 
-          {!contacte ? null : (
-            <p>
-              Contacte: <a href={`mailto:${contacte}`}>{contacte}</a>
-            </p>
-          )}
+                  {!contacte ? null : (
+                      <p>
+                          Contacte: <a href={`mailto:${contacte}`}>{contacte}</a>
+                      </p>
+                  )}
 
-          <div className={styles.grid}>
-            {!per_que ? null : (
-              <div className={styles.card}>
-                <h4>Per què</h4>
-                <p>{per_que}</p>
-              </div>
-            )}
-            {!descripcio ? null : (
-              <div className={styles.card}>
-                <h4>Descripció</h4>
-                <p dangerouslySetInnerHTML={{ __html: descripcio }} />
-              </div>
-            )}
-            {!com ? null : (
-              <div className={styles.card}>
-                <h4>Com</h4>
-                <p>{com}</p>
-              </div>
-            )}
-            {!per_qui ? null : (
-              <div className={styles.card}>
-                <h4>Per qui</h4>
-                <p>{per_qui}</p>
-              </div>
-            )}
-            {!que ? null : (
-              <div className={styles.card}>
-                <h4>Què</h4>
-                <p>{que}</p>
-              </div>
-            )}
-            {!qui ? null : (
-              <div className={styles.card}>
-                <h4>Qui</h4>
-                <section dangerouslySetInnerHTML={{ __html: qui }} />
-              </div>
-            )}
-            {!on ? null : (
-              <div className={styles.card}>
-                <h4>On</h4>
-                <section
-                  className='dont-break-out'
-                  dangerouslySetInnerHTML={{ __html: on }}
-                />
-              </div>
-            )}
+                  <div className={styles.grid}>
+                      {!per_que ? null : (
+                          <div className={styles.card}>
+                              <h4>Per què</h4>
+                              <p>{per_que}</p>
+                          </div>
+                      )}
+                      {!descripcio ? null : (
+                          <div className={styles.card}>
+                              <h4>Descripció</h4>
+                              <p dangerouslySetInnerHTML={{ __html: descripcio }} />
+                          </div>
+                      )}
+                      {!com ? null : (
+                          <div className={styles.card}>
+                              <h4>Com</h4>
+                              <p>{com}</p>
+                          </div>
+                      )}
+                      {!per_qui ? null : (
+                          <div className={styles.card}>
+                              <h4>Per qui</h4>
+                              <p>{per_qui}</p>
+                          </div>
+                      )}
+                      {!que ? null : (
+                          <div className={styles.card}>
+                              <h4>Què</h4>
+                              <p>{que}</p>
+                          </div>
+                      )}
+                      {!qui ? null : (
+                          <div className={styles.card}>
+                              <h4>Qui</h4>
+                              <section dangerouslySetInnerHTML={{ __html: qui }} />
+                          </div>
+                      )}
+                      {!on ? null : (
+                          <div className={styles.card}>
+                              <h4>On</h4>
+                              <section
+                                  className='dont-break-out'
+                                  dangerouslySetInnerHTML={{ __html: on }}
+                              />
+                          </div>
+                      )}
+                  </div>
+
+                  {!contacte ? null : (
+                      <p>
+                          Contacte: <a href={`mailto:${contacte}`}>{contacte}</a>
+                      </p>
+                  )}
+
+                  <div className={styles.socialShareIcons}>
+                      <div className={styles.PostSomeNetwork}>
+                          <FacebookShareButton
+                              url={`https://www.aracultura.com/${type}/${id}`}
+                              className='Post__some-network__share-button'
+                          >
+                              <FacebookIcon size={25} round />
+                          </FacebookShareButton>
+                      </div>
+
+                      <div className={styles.PostSomeNetwork}>
+                          <TwitterShareButton
+                              url={`https://www.aracultura.com/${type}/${id}`}
+                              title={title}
+                              hashtags={['aracultura']}
+                              via='AdhocCultura'
+                              className='Post__some-network__share-button'
+                          >
+                              <TwitterIcon size={25} round />
+                          </TwitterShareButton>
+                      </div>
+
+                      <div className={styles.PostSomeNetwork}>
+                          <LinkedinShareButton
+                              url={`https://www.aracultura.com/${type}/${id}`}
+                              title={title}
+                              className='Post__some-network__share-button'
+                          >
+                              <LinkedinIcon size={25} round />
+                          </LinkedinShareButton>
+                      </div>
+
+                      <div className={styles.PostSomeNetwork}>
+                          <WhatsappShareButton
+                              url={`https://www.aracultura.com/${type}/${id}`}
+                              title={title}
+                              className='Post__some-network__share-button'
+                          >
+                              <WhatsappIcon size={25} round />
+                          </WhatsappShareButton>
+                      </div>
+
+                      <div className={styles.PostSomeNetwork}>
+                          <EmailShareButton
+                              url={`https://www.aracultura.com/${type}/${id}`}
+                              subject={title}
+                              body={`Fes-li un cop d'ull a ${title} https://www.aracultura.com/${type}/${id}`}
+                              className='Post__some-network__share-button'
+                          >
+                              <EmailIcon size={25} round />
+                          </EmailShareButton>
+                      </div>
+                  </div>
+                  <h3>
+                      {paths
+                          .sort((a, b) => {
+                              if (a.id > b.id) {
+                                  return -1;
+                              }
+                              if (a.id < b.id) {
+                                  return 1;
+                              }
+                              return 0;
+                          })
+                          .map((c, id) => (
+                              <Link key={id} href={c}>
+                                  <a className={styles.paths}>{id}</a>
+                              </Link>
+                          ))}
+                  </h3>
+                  {type == 'recursos' || type == 'post' ? (
+                      type == 'recursos' ? (
+                          <Link href={`/inspirat`}>
+                              <a>
+                                  <h3>&larr; Tornar</h3>
+                              </a>
+                          </Link>
+                      ) : (
+                          <Link href={`/actualitat`}>
+                              <a>
+                                  <h3>&larr; Tornar</h3>
+                              </a>
+                          </Link>
+                      )
+                  ) : (
+                      <Link href={`/${type}`}>
+                          <a>
+                              <h3>&larr; Tornar</h3>
+                          </a>
+                      </Link>
+                  )}
+              </IntlProvider>
           </div>
-
-          {!contacte ? null : (
-            <p>
-              Contacte: <a href={`mailto:${contacte}`}>{contacte}</a>
-            </p>
-          )}
-
-          <div className={styles.socialShareIcons}>
-            <div className={styles.PostSomeNetwork}>
-              <FacebookShareButton
-                url={`https://www.aracultura.com/${type}/${id}`}
-                className='Post__some-network__share-button'>
-                <FacebookIcon size={25} round />
-              </FacebookShareButton>
-            </div>
-
-            <div className={styles.PostSomeNetwork}>
-              <TwitterShareButton
-                url={`https://www.aracultura.com/${type}/${id}`}
-                title={title}
-                hashtags={['aracultura']}
-                via='AdhocCultura'
-                className='Post__some-network__share-button'>
-                <TwitterIcon size={25} round />
-              </TwitterShareButton>
-            </div>
-
-            <div className={styles.PostSomeNetwork}>
-              <LinkedinShareButton
-                url={`https://www.aracultura.com/${type}/${id}`}
-                title={title}
-                className='Post__some-network__share-button'>
-                <LinkedinIcon size={25} round />
-              </LinkedinShareButton>
-            </div>
-
-            <div className={styles.PostSomeNetwork}>
-              <EmailShareButton
-                url={`https://www.aracultura.com/${type}/${id}`}
-                subject={title}
-                body={`Fes-li un cop d'ull a ${title} https://www.aracultura.com/${type}/${id}`}
-                className='Post__some-network__share-button'>
-                <EmailIcon size={25} round />
-              </EmailShareButton>
-            </div>
-          </div>
-          <h3>
-            {paths
-              .sort((a, b) => {
-                if (a.id > b.id) {
-                  return -1;
-                }
-                if (a.id < b.id) {
-                  return 1;
-                }
-                return 0;
-              })
-              .map((c, id) => (
-                <Link key={id} href={c}>
-                  <a className={styles.paths}>{id}</a>
-                </Link>
-              ))}
-          </h3>
-          {type == 'recursos' || type == 'post' ? (
-            type == 'recursos' ? (
-              <Link href={`/inspirat`}>
-                <a>
-                  <h3>&larr; Tornar</h3>
-                </a>
-              </Link>
-            ) : (
-              <Link href={`/actualitat`}>
-                <a>
-                  <h3>&larr; Tornar</h3>
-                </a>
-              </Link>
-            )
-          ) : (
-            <Link href={`/${type}`}>
-              <a>
-                <h3>&larr; Tornar</h3>
-              </a>
-            </Link>
-          )}
-        </IntlProvider>
-      </div>
-    </>
+      </>
   );
 };
 
