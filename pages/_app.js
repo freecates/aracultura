876bd4d9f1e0bb4fbdd422e0f11351317dbf74e8
ramon.gil <ramon.gil@deporvillage.com>
2020-04-28 09:06:06 +0200

    added gtag via lib

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 import '../public/styles.css';
 import App from "next/app";
 import { AnimatePresence } from "framer-motion";
+import Router from 'next/router'
+
+import * as gtag from '../lib/gtag'
+
+Router.events.on('routeChangeComplete', (url) => gtag.pageview(url))
 
 class MyApp extends App {
   render() {
