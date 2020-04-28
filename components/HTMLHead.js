b27d72b403e98ee4fa5b29dee4d0be8fc882b547
ramon.gil <ramon.gil@deporvillage.com>
2020-04-28 09:23:41 +0200

    analytics via double script

--- components/HTMLHead.js
+++ components/HTMLHead.js
@@ -57,14 +57,16 @@ const HTMLHead = ({ title, description, page, slug }) => (
             }}
         />
         <link rel='preconnect' href='https://www.google-analytics.com' crossOrigin={'true'} />
-        <link rel='preconnect' href='https://www.googletagmanager.com' crossOrigin={'true'} />
+
+        <script async src='https://www.googletagmanager.com/gtag/js?id=UA-47226335-9'></script>
+
         <script
             dangerouslySetInnerHTML={{
-                __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','UA-47226335-9');`,
+                __html: `window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+              
+                gtag('config', 'UA-47226335-9');`,
             }}
         ></script>
     </Head>
