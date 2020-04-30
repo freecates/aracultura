080307153672bf94dd3c8e9b6c3c936e5d07a628
ramon.gil <ramon.gil@deporvillage.com>
2020-04-30 18:22:59 +0200

    added banners

---
+++ components/Banners.js
@@ -0,0 +1,62 @@
+import styles from './Banners.module.scss';
+import { useMediaQuery } from 'react-responsive';
+
+const Banners = ({ banners }) => {
+    let date = new Date().toLocaleDateString();
+    const bannersActius = banners.filter(
+        (x) =>
+            new Date(x.acf.data_inici_campanya).toLocaleDateString() == date || 
+            new Date(x.acf.data_finalitzacio_campanya).toLocaleDateString() >= date
+    );
+    const isDesktopOrLaptop = useMediaQuery({
+        query: '(min-device-width: 768px)',
+    });
+    const isMobileDevice = useMediaQuery({
+        query: '(max-device-width: 767px)',
+    });
+    return (
+        <>
+            <div className={styles.banners}>
+                {bannersActius
+                    .sort((a, b) => {
+                        if (a.acf.data_inici_campanya > b.acf.data_inici_campanya) {
+                            return -1;
+                        }
+                        if (a.acf.data_inici_campanya < b.acf.data_inici_campanya) {
+                            return 1;
+                        }
+                        return 0;
+                    })
+                    .map((b, id) => (
+                        <a
+                            href={b.acf.url_de_desti}
+                            title={b.acf.banner_dk.title}
+                            target={'_blank'}
+                            key={id}
+                        >
+                            <figure>
+                                {isDesktopOrLaptop && (
+                                    <img
+                                        src={`${b.acf.banner_dk.url}`}
+                                        loading={'lazy'}
+                                        width={b.acf.banner_dk.width}
+                                        height={b.acf.banner_dk.height}
+                                    />
+                                )}
+                                {isMobileDevice && (
+                                    <img
+                                        src={`${b.acf.banner_mobile.url}`}
+                                        loading={'lazy'}
+                                        width={b.acf.banner_mobile.width}
+                                        height={b.acf.banner_mobile.height}
+                                    />
+                                )}
+                            </figure>
+                        </a>
+                    ))}
+            </div>
+        </>
+    );
+};
+
+export default Banners;
--- pages/index.js
+++ pages/index.js
@@ -2,35 +2,40 @@ import fetch from 'isomorphic-unfetch';
 import HTMLHead from '../components/HTMLHead';
 import Layout from '../components/Layout';
 import { motion } from 'framer-motion';
+import Banners from '../components/Banners';
 
-const Home = ({ data }) => {
-  const { title, content, excerpt } = data;
-  return (
-    <motion.div initial='initial' animate='animate' exit={{ opacity: 0 }}>
-      <HTMLHead
-        title={title.rendered}
-        description={excerpt.rendered.replace(/(<([^>]+)>)/gi, '')}
-        page={'home'}
-      />
-      <Layout page={'home'}>
-        <h1 className='title'>{title.rendered}</h1>
-        <div
-          className='description'
-          dangerouslySetInnerHTML={{ __html: content.rendered }}
-        />
-      </Layout>
-    </motion.div>
-  );
+const Home = ({ data, banners }) => {
+    const { title, content, excerpt } = data;
+    return (
+        <motion.div initial='initial' animate='animate' exit={{ opacity: 0 }}>
+            <HTMLHead
+                title={title.rendered}
+                description={excerpt.rendered.replace(/(<([^>]+)>)/gi, '')}
+                page={'home'}
+            />
+            <Layout page={'home'}>
+                {!banners ? null : <Banners banners={banners} />}
+                <h1 className='title'>{title.rendered}</h1>
+                <div
+                    className='description'
+                    dangerouslySetInnerHTML={{ __html: content.rendered }}
+                />
+            </Layout>
+        </motion.div>
+    );
 };
 
 export const getStaticProps = async () => {
-  const res = await fetch(`https://cms.aracultura.com/wp-json/wp/v2/pages/76`);
-  const data = await res.json();
-  return {
-    props: {
-      data: data,
-    },
-  };
+    const res = await fetch(`https://cms.aracultura.com/wp-json/wp/v2/pages/76`);
+    const data = await res.json();
+    const res2 = await fetch(`https://cms.aracultura.com/wp-json/acf/v3/banners`);
+    const data2 = await res2.json();
+    return {
+        props: {
+            data: data,
+            banners: data2,
+        },
+    };
 };
 
 export default Home;
