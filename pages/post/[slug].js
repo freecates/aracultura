a7026db1a2abc73169194da5dee672c41f7a92b6
ramon.gil <ramon.gil@deporvillage.com>
2020-07-17 08:27:00 +0200

    ISG in posts

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,25 +1,34 @@
+import { useRouter } from 'next/router';
+import ErrorPage from 'next/error';
 import BlocPost from '../../components/BlocPost';
 import Layout from '../../components/Layout';
 import { motion } from 'framer-motion';
 
 const Post = ({ post, paths }) => {
+    const router = useRouter();
 
-    const { title, acf, type, id, date, author, content, slug } = post;
+    if (!router.isFallback && !post?.slug) {
+        return <ErrorPage title={'No el tenim aquest post'} />;
+    }
 
     return (
         <motion.div initial='initial' animate='animate' exit={{ opacity: 0 }}>
             <Layout>
-                <BlocPost
-                    title={title.rendered}
-                    description={acf.destacat}
-                    id={id}
-                    type={type}
-                    content={content.rendered}
-                    paths={paths}
-                    date={date}
-                    author={author}
-                    slug={slug}
-                />
+                {router.isFallback ? (
+                    <p>Carregant…</p>
+                ) : (
+                    <BlocPost
+                        title={post.title.rendered}
+                        description={post.acf.destacat}
+                        id={post.id}
+                        type={post.type}
+                        content={post.content.rendered}
+                        paths={paths}
+                        date={post.date}
+                        author={post.author}
+                        slug={post.slug}
+                    />
+                )}
             </Layout>
             <style jsx global>{`
                 .container {
@@ -30,15 +39,6 @@ const Post = ({ post, paths }) => {
     );
 };
 
-export async function getStaticPaths() {
-    const res = await fetch('https://cms.aracultura.com/wp-json/wp/v2/posts');
-    const posts = await res.json();
-
-    const paths = posts.map((post) => `/${post.type}/${post.slug}`);
-
-    return { paths, fallback: false };
-}
-
 export async function getStaticProps({ params }) {
     const res = await fetch(`https://cms.aracultura.com/wp-json/wp/v2/posts`);
     const dataRes = await res.json();
@@ -46,6 +46,15 @@ export async function getStaticProps({ params }) {
     const paths = dataRes.map((dataRe) => `/${dataRe.type}/${dataRe.slug}`);
 
     return { props: { post: dataRes.find((x) => x.slug == params.slug), paths } };
+}
+
+export async function getStaticPaths() {
+    const res = await fetch('https://cms.aracultura.com/wp-json/wp/v2/posts');
+    const posts = await res.json();
+
+    const paths = posts.map((post) => `/${post.type}/${post.slug}`)  || [];
+
+    return { paths, fallback: true };
 }
 
 export default Post;
